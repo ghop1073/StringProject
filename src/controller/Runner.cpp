8d6be30678ab894d9ee909fa8e2e5e3a2599d6ec
ghop1073 <mhop1073@740s-pl-109.local>
2016-02-24 11:25:12 -0700

    just forgot to add to github
    forgot runner and added to project then added to repo

---
+++ src/controller/Runner.cpp
@@ -0,0 +1,16 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: mhop1073
+ */
+
+#include "StringController.h"
+
+int main()
+{
+	StringController * baseController = new StringController();
+	baseController->start();
+
+	return 0;
+}
---
+++ src/controller/StringController.h
@@ -0,0 +1,27 @@
+/*
+ * StringController.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: mhop1073
+ */
+
+#ifndef CONTROLLER_STRINGCONTROLLER_H_
+#define CONTROLLER_STRINGCONTROLLER_H_
+#include <string>
+using namespace std;
+
+class StringController
+{
+private:
+
+public:
+
+	StringController();
+	string printThis;
+
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_STRINGCONTROLLER_H_ */
